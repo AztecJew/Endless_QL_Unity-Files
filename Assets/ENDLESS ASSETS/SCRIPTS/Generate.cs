4b23624dd956bfdf4970f799498ccaac9c1ef41c
AztecJew <ryan.sams@nyfa.edu>
2015-03-02 18:15:56 -0500

    Initial
    FIles

---
+++ Assets/ENDLESS ASSETS/SCRIPTS/Generate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+	public GameObject booze;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("createObstacle", Random.Range(1f, 3f), Random.Range(1f, 3f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createObstacle() {
+		Instantiate (booze);
+		score++;
+		print(score);
+		if(score > 5) {
+			CancelInvoke();
+			InvokeRepeating("createObstacle", Random.Range(1f, 8f), Random.Range(1f, 8f));
+		}
+		else {
+			CancelInvoke();
+			InvokeRepeating("createObstacle", Random.Range(1f, 3f), Random.Range(1f, 3f));
+		}
+	}
+}
---
+++ Assets/ENDLESS ASSETS/SCRIPTS/Jump.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	private int track = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			track = track + 1;
+			if(track < 2) {
+				rigidbody2D.transform.position = new Vector3(0, rigidbody2D.transform.position.y + 4, -1);
+			}
+		}
+	}
+}
